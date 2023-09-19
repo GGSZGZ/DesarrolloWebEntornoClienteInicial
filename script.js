63dce9a8ac0bbce07f62d70aa1937c45d10b6930
Guillermo García <guillermo.garcia@fundacionmontessori.com>
2023-09-19 21:02:53 +0200

    Version entrega

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-let menu=document.querySelector('.header_more');
 
-if (window.innerWidth <= 600) {
-    menu.display('none');
+function hideMenuItems() {
+
+const homeLink = document.getElementById('home-link');
+    const aboutLink = document.getElementById('about-link');
+    const projectsLink = document.getElementById('projects-link');
+    const contactLink = document.getElementById('contact-link');
+
+const windowWidth = window.innerWidth;
+
+const breakpointWidth = 600;
+
+if (windowWidth < breakpointWidth) {
+    aboutLink.style.display = 'none';
+    projectsLink.style.display = 'none';
+    contactLink.style.display = 'none';
+    homeLink.style.display = 'none';
+
 } else {
-   
+    projectsLink.style.display = 'inline-block';
+    contactLink.style.display = 'inline-block';
+    aboutLink.style.display = 'inline-block';
+    homeLink.style.display = 'inline-block';
+
 }
+}
+hideMenuItems();
+
+
+window.addEventListener('resize', hideMenuItems);
